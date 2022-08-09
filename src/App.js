66cd8bc1c7c2dfafcb66559d70710fdd005c8004
Ahmad Tariq <ahmad.tariq@cbc-extern.de>
2022-08-09 20:53:39 +0200

    Add dragdrop component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import { NavbarSimple } from './Components/Navbar'
+import { DndListHandle } from './Components/dndlist'
+import { Container, Grid } from '@mantine/core';
 
 function App() {
+  const data = [
+    { "position": 6, "mass": 12.011, "symbol": "C", "name": "Carbon" },
+    { "position": 7, "mass": 14.007, "symbol": "N", "name": "Nitrogen" },
+    { "position": 39, "mass": 88.906, "symbol": "Y", "name": "Yttrium" },
+    { "position": 56, "mass": 137.33, "symbol": "Ba", "name": "Barium" },
+    { "position": 58, "mass": 140.12, "symbol": "Ce", "name": "Cerium" }
+  ]
   return (
     <div className="App">
-      <NavbarSimple />
+      <Grid>
+        <Grid.Col span={3}>
+          <NavbarSimple />
+        </Grid.Col>
+        <Grid.Col span={8}>
+          <Container>
+            <DndListHandle data={data} />
+          </Container>
+        </Grid.Col>
+      </Grid>
+
+
     </div>
   );
 }
